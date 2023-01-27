ff0de64233172c53c656c30f550ff5c6a26d8f77
teosilva72 <representate.com.sp@gmail.com>
2023-01-26 21:19:00 -0300

    modified:   .gitignore 	modified:   src/views/pages/login.ejs 	new file:   src/views/pages/modal/createUser.ejs 	modified:   src/views/src/js/login.js 	new file:   src/views/src/js/md5.js

--- src/views/src/js/login.js
+++ src/views/src/js/login.js
@@ -1,3 +1,10 @@
+let ip = localStorage.getItem("ip")
+ip = ip.split(":");
+const token = () => {
+  let token = document.cookie;
+  token = token.split('=');
+  return token;
+}
 
 $(() => {
   $('#formLogin').on('submit', (event) => {
@@ -6,3 +13,42 @@ $(() => {
     });
   });
 });
+
+function free() {
+  var pass = prompt("Digite a senha de acesso.");
+  if (pass == 'admin') $('#btnRegister').click();
+  else alert('Senha invalida.');
+}
+
+function postUser() {
+  
+  var url = `${ip[0]}:${ip[1]}:3005/user-register`;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+
+  //xhr.setRequestHeader("Authorization", `${token()[0]} ${token()[1]}`);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 401) alert401('msgUser', `Erro ao salvar produto! \nError: ${xhr.responseText}`);
+      else if (xhr.status === 201) alert200('msgUser', `Usuário salvo com sucesso!`);
+    }
+  };
+
+  var data = `
+      {
+        "name": "${$('.name').val()}",
+        "nickName": "${$('.nickName').val()}",
+        "email": "${$('.email').val()}",
+        "password": "${$('.password').val()}",
+        "cell": "${$('.cell').val()}",
+        "sex": "${$('.sex').val()}",
+        "birth": "${$('.birth').val()}",
+        "sex":"${$('.sex').val()}"
+      }
+  `;
+
+  xhr.send(data);
+}
