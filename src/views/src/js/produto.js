2e27cb6188072528771a35a2944fa020dd542968
teosilva72 <representate.com.sp@gmail.com>
2023-02-01 08:11:38 -0300

    modified:   src/views/pages/modal/produto.ejs 	modified:   src/views/pages/produto.ejs 	modified:   src/views/src/js/produto.js

--- src/views/src/js/produto.js
+++ src/views/src/js/produto.js
@@ -67,7 +67,13 @@ function getProduct() {
 getProduct(); // init table
 
 function titleModal(data) {
-    if (data == 1) $("#titleModal").text("Novo Registro de Produto");
+    if (data == 1) {
+        $("#titleModal").text("Novo Registro de Produto");
+        $('#formProduto input, textarea').attr('disabled', false);
+        $('.categoria, #saveProduct').removeClass('d-none');
+        $('.categoria-one').addClass('d-none');
+        $('#formProduto select, input, textarea').val('')
+    }
     else if (data == 2) $("#titleModal").text("Descrição do Produto");
 }
 
@@ -158,17 +164,16 @@ function postProduct() {
             "Authorization": `${token()[0]} ${token()[1]}`
         },
         success: (res) => {
-            $('#formProduto, select, input').val(''); // reset form
+            $('#formProduto, select, input, textarea').val(''); // reset form
             $('.exit').click(); //fecha modal
-            console.log(res.data)
             let table = $('#tableProduto').dataTable();
-            //$('#tableProduto').dataTable().fnClearTable();
-            const data2 = res.data
-            
+            let data2 = res.data
+            data2.categoria = 'teste'
             table.dataTable().fnAddData(data2); // add bew row
-            $('#formProduto, select, input').val(''); // reset form
-            console.log(data2)
-            alert200('msgAction', res.message); //apresenta mensagem
+            setTimeout(() => {
+                $('#formProduto, select, input').val(''); // reset form
+                alert200('msgAction', res.message); //apresenta mensagem
+            }, 500);
         }, error: (res) => {
             const msg = res.responseJSON;
             alert401('alertPostProduct', `${msg.data}!`)
@@ -176,13 +181,13 @@ function postProduct() {
     });
 }
 
-function colorDark(id){
+function colorDark(id) {
     let table = $('#tableProduto');
     table.addClass('table-dark');
     $(`#${id}`).addClass('d-none');
     $('#colorlight').removeClass('d-none');
 }
-function colorLight(id){
+function colorLight(id) {
     let table = $('#tableProduto');
     table.removeClass('table-dark');
     $(`#${id}`).addClass('d-none');
@@ -200,17 +205,126 @@ function viewProduct(id) {
             "Authorization": `${token()[0]} ${token()[1]}`
         },
         success: (res) => {
-           console.log(res.data)
+            $('.nome').val(res.data[0].nome);
+            $('.marca').val(res.data[0].marca);
+            $('.modelo').val(res.data[0].modelo);
+            $('.descricao').val(res.data[0].descricao);
+            $('.cod_barras').val(res.data[0].cod_barras);
+            $('.dt_fabricacao').val(res.data[0].dt_fabricacao);
+            $('.dt_validade').val(res.data[0].dt_validade);
+            $('.fabricante').val(res.data[0].fabricante);
+            $('.localizacao').val(res.data[0].localizacao);
+            $('.quantidade').val(res.data[0].quantidade);
+            $('.valor').val(res.data[0].valor);
+            $('.valor_venda').val(res.data[0].valor_venda);
+
+            setTimeout(() => {
+                $('#modalProduto').modal('show');
+                $('#formProduto input, textarea').attr('disabled', true);
+                $('.categoria, #saveProduct').addClass('d-none');
+                $('#input-categoria').append(`
+                    <input type="text" value="${res.data[0].category[0].name}" class="categoria-one form-control mb-3" disabled>
+                `);
+                $('#editProduct').addClass('d-none');
+                $('#titleModal').text('Visualização Produto');
+            }, 1000)
         }, error: (res) => {
-            console.log(res.data + " <<< error")
+            alert(res);
         }
     });
 }
 
-function editProduct(id){
-    alert('edit' + id);
+function editProduct(id) {
+    const url = `${ip[0]}:${ip[1]}:3005/get-product/${id}`;
+    $.ajax({
+        type: 'GET',
+        url: url,
+        contentType: 'application/json',
+        async: false,
+        headers: {
+            "Authorization": `${token()[0]} ${token()[1]}`
+        },
+        success: (res) => {
+            getCategory();
+            //$('.categoria').val().change();
+            
+            $('.nome').val(res.data[0].nome);
+            $('.marca').val(res.data[0].marca);
+            $('.modelo').val(res.data[0].modelo);
+            $('.descricao').val(res.data[0].descricao);
+            $('.cod_barras').val(res.data[0].cod_barras);
+            $('.dt_fabricacao').val(res.data[0].dt_fabricacao);
+            $('.dt_validade').val(res.data[0].dt_validade);
+            $('.fabricante').val(res.data[0].fabricante);
+            $('.localizacao').val(res.data[0].localizacao);
+            $('.quantidade').val(res.data[0].quantidade);
+            $('.valor').val(res.data[0].valor);
+            $('.valor_venda').val(res.data[0].valor_venda);
+
+            setTimeout(() => {
+                
+                $('#modalProduto').modal('show');
+                $('.categoria').removeClass('d-none');
+                $('#formProduto input, textarea').attr('disabled', false);
+                $(`.categoria option[value=${res.data[0].category[0]._id}]`).attr('selected','selected');
+                $('.categoria-one').addClass('d-none');
+                $('#saveProduct').addClass('d-none');
+                $('#titleModal').text('Editar Produto');
+                $('#editProduct').removeClass('d-none');
+                $('#idProduct').val(`${res.data[0]._id}`)
+                idProduct = res.data[0]._id
+            }, 1000)
+        }, error: (res) => {
+            alert(res);
+        }
+    });
 }
 
-function removeProduct(id){
+let idProduct = '';
+
+function PutProduct(){
+    var url = `${ip[0]}:${ip[1]}:3005/product-edit/${idProduct}`;
+
+    var data = {
+        categoria: `${$('.categoria').val()}`,
+        nome: `${$('.nome').val()}`,
+        marca: `${$('.marca').val()}`,
+        modelo: `${$('.modelo').val()}`,
+        descricao: `${$('.descricao').val()}`,
+        cod_barras: `${$('.cod_barras').val()}`,
+        dt_fabricacao: `${$('.dt_fabricacao').val()}`,
+        dt_validade: `${$('.dt_validade').val()}`,
+        fabricante: `${$('.fabricante').val()}`,
+        localizacao: `${$('.localizacao').val()}`,
+        quantidade: `${$('.quantidade').val()}`,
+        valor: `${$('.valor').val()}`,
+        valor_venda: `${$('.valor_venda').val()}`
+    };
+    
+    $.ajax({
+        type: 'PUT',
+        url: url,
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        async: false,
+        headers: {
+            "Authorization": `${token()[0]} ${token()[1]}`
+        },
+        success: (res) => {
+            $('#formProduto, select, input, textarea').val(''); // reset form
+            $('.exit').click(); //fecha modal
+            alert200('msgAction', res.message); //apresenta mensagem
+        }, error: (res) => {
+            const msg = res.message;
+            alert401('alertPostProduct', `${msg}!`)
+        }
+    });
+}
+
+function removeProduct(id) {
     alert('delete' + id);
+}
+
+function addCategoria() {
+    $('#inputNewCategory').attr('disabled', false);
 }
